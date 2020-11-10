d00eaa11eeaa774fedf9bae635f98e9e09cfc451
Pedro Vitor <pedrocavt@gmail.com>
2020-11-09 21:26:19 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,45 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'cp1.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'cp2.jpg', legenda: 'Vereador' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'cp3.jpg', legenda: "Prefeito" },
+                    { url: 'cp4.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulu',
+                partido: 'QUERTY',
+                fotos: [
+                    { url: 'cp5.jpg', legenda: "Prefeito" },
+                    { url: 'cp6.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ]
+    }
+]
